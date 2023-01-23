50d4d2c16964bf7036998d2abbfc3bb49fa23b28
chanel donahoe <cdonahoe05@gmail.com>
2023-01-24 10:28:48 +1100

    updates

--- README.md
+++ README.md
@@ -15,5 +15,15 @@
 - Higher Order Functions - 
 - Change padding at the top of each section (it is too big at the moment)
 - fix dynamicness 
-- fix skills and experience order 
+- fix skills and experience order (with list of experience in year order)
 - replace github with proper github in the social media section (github, linked in links)
+- fix navbar change skills to experience and link correctly 
+- Change mobile icon to linked in image in footer 
+
+
+
+
+// stretch
+- add authentication to contact form
+  - make sure the email is an actual email 
+  - add max length in the contact form 
--- frontend_react/src/container/Footer/footer.jsx
+++ frontend_react/src/container/Footer/footer.jsx
@@ -1,28 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
-import { images } from '../../constants';
 import './footer.scss';
 
 const Footer = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { username, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-      <h2 className="head-text">Lets have a chat!</h2>
-
+      <h2 className="head-text">Let's have a  chat</h2>
 
       <div className="app__footer-cards">
-        <div className="app__footer-card">
+        <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:cdonahoe05#gmail.com" className='p-text'>cdonahoe05@gmail.com</a>
-          <a href="https://www.linkedin.com/in/chanel-donahoe/">Message me on linkedin</a>
+          <a href="mailto:cdonahoe05@gmail.com" className="p-text">cdonahoe05@gmail.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="linkedin" />
+          <a href="https://www.linkedin.com/in/chanel-donahoe/" className="p-text">Linkedin</a>
         </div>
       </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
-  'app-whitebg'
-)
+  'app__whitebg',
+);
