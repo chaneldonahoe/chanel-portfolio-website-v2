4bb4362ad4bb78f0f68a7e0f5217e382f5454d8e
chanel donahoe <cdonahoe05@gmail.com>
2023-01-24 09:51:38 +1100

    new branch

--- frontend_react/src/container/Footer/footer.jsx
+++ frontend_react/src/container/Footer/footer.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+import { images } from '../../constants';
 import './footer.scss';
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
+    <>
+      <h2 className="head-text">Lets have a chat!</h2>
+
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:cdonahoe05#gmail.com" className='p-text'>cdonahoe05@gmail.com</a>
+          <a href="https://www.linkedin.com/in/chanel-donahoe/">Message me on linkedin</a>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app-whitebg'
+)
