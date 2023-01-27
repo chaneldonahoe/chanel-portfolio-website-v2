f0dd05f7bded58b70739a74556d9442fcc15d8c4
chanel donahoe <cdonahoe05@gmail.com>
2023-01-27 13:27:29 +1100

    update to footer

--- frontend_react/src/container/Footer/footer.jsx
+++ frontend_react/src/container/Footer/footer.jsx
@@ -12,6 +12,7 @@ const Footer = () => {
   const [validName, setValidName] = useState(true);
   const [validMessage, setValidMessage] = useState(true);
   const { username, email, message } = formData;
+  // eslint-disable-next-line 
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const validateEmail = (email) => {
